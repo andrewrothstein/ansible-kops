f4379b9c6c02ad5c5826355424c5a5e3778dd72c
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-01-13 20:55:10 -0500

    up to v1.11.0. dcb to v0.0.16. delint.

---
+++ dl-checksum.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+VER=1.11.0
+DIR=~/Downloads
+MIRROR=https://github.com/kubernetes/kops/releases/download
+
+dl()
+{
+    OS=$1
+    PLATFORM=$2
+    printf "$OS-$PLATFORM: sha1:"
+    curl -SsL $MIRROR/$VER/kops-$OS-$PLATFORM-sha1
+}
+
+dl linux amd64
+dl darwin amd64
+dl windows amd64
