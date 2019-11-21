45c4b9e569ab19f180023d56b213652b0d4efdd4
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-21 08:04:52 -0500

    up to v1.15.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env sh
-VER=${1:-1.14.1}
-DIR=~/Downloads
 MIRROR=https://github.com/kubernetes/kops/releases/download
 
 dl()
 {
-    os=$1
-    arch=$2
-    platform=${os}-${arch}
-    url=$MIRROR/$VER/kops-$platform-sha1
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local platform="${os}-${arch}"
+    local url=$MIRROR/$ver/kops-$platform-sha256
     printf "    # %s\n" $url
-    printf "    %s: sha1:%s\n" $platform `curl -SsL $url`
+    printf "    %s: sha256:%s\n" $platform `curl -SsL $url`
 }
 
-printf "  '%s':\n" $VER
-dl linux amd64
-dl darwin amd64
-dl windows amd64
+dl_ver()
+{
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver linux amd64
+    dl $ver darwin amd64
+    dl $ver windows amd64
+}
+
+dl_ver ${1:-1.15.0}
