8e46d8a67df69df47476f9a66bc66ea6fc494600
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-04-24 10:55:16 -0400

    kops:1.23.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -10,7 +10,7 @@ dl()
     local platform="${os}-${arch}"
     local url="$MIRROR/$ver/kops-${platform}.sha256"
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform `curl -SsL $url`
+    printf "    %s: sha256:%s\n" $platform $(curl -SsLf $url)
 }
 
 dl_ver()
@@ -18,8 +18,10 @@ dl_ver()
     local ver=$1
     printf "  %s:\n" $ver
     dl $ver linux amd64
+    dl $ver linux arm64
     dl $ver darwin amd64
+    dl $ver darwin arm64
     dl $ver windows amd64
 }
 
-dl_ver ${1:-v1.23.0}
+dl_ver ${1:-v1.23.1}
